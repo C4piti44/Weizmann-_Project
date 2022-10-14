5d0a8c9144fdb5478819a6b0cb0ec04b54e34016
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-10-14 21:15:56 +0300

    Update reco_v2_f.py

--- reco_v2_f.py
+++ reco_v2_f.py
@@ -1,19 +1,24 @@
 import cv2
 import numpy as np
 
+# This function dedect the object's vertices.
+# If the the number of vertices is suitable to the predefine number of vertices,
+# the function will highlight the contours.
+
 def finding_shape(img , contour):
     approx  = cv2.approxPolyDP(contour , 0.01*cv2.arcLength(contour , True) , True)
     if 5 > len(approx) > 2:
         cv2.drawContours(img, [approx], 0,(255,0,0),4)
         cv2.putText( img , "object has been detected" , (50,50) ,font, 1 , (255,255,255))
 
-# this function de
+# This function dedect the object in the space.
+# The function draw a rectangle on the dedeted object. 
+
 def object_position(img, contour):
     x ,y, w ,h = cv2.boundingRect(contour)
     print(f"({x},{y})")
     cv2.rectangle(img , (x,y) , (x+w , y+h ) , (0,0,255))
     cv2.putText(img, 'follow', (x - 60, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
-
 
 
 font = cv2.FONT_HERSHEY_SIMPLEX
