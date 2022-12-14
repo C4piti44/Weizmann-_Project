d303b0235a1002353b2ddfbb4027c77ebf359858
C4piti44 <97689075+C4piti44@users.noreply.github.com>
2022-12-14 17:04:04 +0200

    Update reco.py

--- reco.py
+++ reco.py
@@ -1,6 +1,29 @@
 import cv2
 import numpy as np
 
+# This function dedect the object's vertices.
+# If the the number of vertices is suitable to the predefine number of vertices,
+# the function will highlight the contours.
+
+def finding_shape(img , contour):
+    approx  = cv2.approxPolyDP(contour , 0.01*cv2.arcLength(contour , True) , True)
+    if 5 > len(approx) > 2:
+        cv2.drawContours(img, [approx], 0,(255,0,0),4)
+        cv2.putText( img , "object has been detected" , (50,50) ,font, 1 , (255,255,255))
+
+# This function dedect the object in the space.
+# The function draw a rectangle on the dedeted object. 
+
+def get_coords(contour):
+     return cv2.boundingRect(contour)
+
+def object_position(img, contour):
+    x ,y, w ,h = cv2.boundingRect(contour)
+    print((cv2.boundingRect(contour)))
+    print(f"({x},{y})")
+    cv2.rectangle(img , (x,y) , (x+w , y+h ) , (0,0,255))
+    cv2.putText(img, 'follow', (x - 60, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
+
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 lower = np.array([30,100,20])
@@ -18,21 +41,11 @@ while True:
     if len(contours)!=0:
         for contour in contours:
             if cv2.contourArea(contour) > 300:
-                
-#this detection is for finding the shape of the tracked object. 
-               
-                approx  = cv2.approxPolyDP(contour , 0.01*cv2.arcLength(contour , True) , True)
-                if 5> len(approx) >2:
-                    cv2.drawContours(img, [approx], 0,(255,0,0),4)
-                    cv2.putText( img , "object has been detected" , (50,50) ,font, 1 , (255,255,255))
+                finding_shape(img , contour)
+                object_position(img , contour)
+                coords = get_coords(contour)
 
-#this detection is for calculating the distance and for knowing how much the robot needs to turn.
-                
-                x ,y, w ,h = cv2.boundingRect(contour)
-                print(f"({x},{y})")
-                cv2.rectangle(img , (x,y) , (x+w , y+h ) , (0,0,255))
-                cv2.putText(img, 'follow', (x - 60, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
-
+            
     cv2.imshow("mask" , mask)
     cv2.imshow("webcam" , img)
 
